150025a0b1a000dc801c281bdc19aa42aa1482e7
Michael Alt <lean@michael-alt.info>
2021-11-30 06:05:15 +0100

    Add ng-bootstrap.  Remove bootstrap scripts.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule],
   providers: [],
+  imports: [BrowserModule, NgbModule, RouterModule.forRoot(appRoutes)],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
